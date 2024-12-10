ee01d47cdb6f8a71b6f611dd8d35aab63bb0eeb6
Jade Lee <jadelee0924@gmail.com>
2024-12-10 14:33:35 -0600

    chore: apply tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
 import StyledComponentsRegistry from '@/lib/registry';
 import StyledComponentsWrapper from '@/lib/styled-components/StyledComponentsWrapper';
 import { Metadata } from 'next';
-
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'akcse-mb',
   description: 'akcse mb code',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </head>
       <body suppressHydrationWarning>
         <StyledComponentsRegistry>
