1d532b6015f3dc2321c8747f8f50f38235b1b189
Jade Lee <jadelee0924@gmail.com>
2024-12-16 12:53:39 -0500

    chore: update login page
    colour, and sizes for components
    use router to set route for the log in button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,40 +1,44 @@
 'use client';
 
-//import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import * as S from 'app/page.styled';
 
 export default function Login() {
-  //const router = useRouter();
+  const { push } = useRouter();
 
   return (
     <>
       <S.MainContent className="flex flex-col min-h-screen space-y-32">
         <div id="loginHeader" className="font-semibold text-center">
-          <h1 className="text-orange-950 text-2xl">AKCSE MANITOBA</h1>
-          <h1 className="text-orange-950 text-base">Young Generations</h1>
+          <h1 className="text-orange-900 text-2xl">AKCSE MANITOBA</h1>
+          <h1 className="text-orange-900 text-base">Young Generations</h1>
         </div>
 
         <div id="loginMain" className="space-y-7 font-mono">
-          <div id="loginTitle" className="text-left ml-5 space-y-1">
-            <h1 className="text-orange- 950 text-xl">Log In</h1>
+          <div id="loginTitle" className="text-left space-y-1">
+            <h1 className="text-orange- 900 text-xl">Log In</h1>
             <h1 className="text-gray-600 text-base">AKCSE MB Admin Access</h1>
           </div>
 
-          <div id="textInput" className="mx-5 text-xl rounded space-y-3">
+          <div id="textInput" className=" text-xl rounded space-y-3">
             <input
               type="text"
               placeholder="Email"
-              className="border-2 rounded"
+              className="border-2 h-12 p-3"
             />
+            <br />
             <input
               type="password"
               placeholder="Password"
-              className="border-2 rounded"
+              className="border-2 h-12 p-3"
             />
           </div>
 
           <div id="buttonInput" className="flex justify-end">
-            <button className="bg-orange-950 hover:bg-orange-800 text-white py-2 px-4 rounded">
+            <button
+              className="bg-orange-900 hover:bg-orange-800 text-white py-2 px-4 rounded"
+              onClick={() => push('/')}
+            >
               Log In
             </button>
           </div>
