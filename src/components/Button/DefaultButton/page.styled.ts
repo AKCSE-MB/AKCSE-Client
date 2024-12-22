cbb6c9625f86fefe8de092b70ebef3495df65252
Jade Lee <jadelee0924@gmail.com>
2024-12-22 12:06:58 -0500

    feat: create a default button component

---
+++ src/components/Button/DefaultButton/page.styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 1rem;
+  background-color: #121212;
+  color: white;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(102, 46, 11);
+  }
+
+  &:disabled {
+    opacity: 0.4;
+    pointer-events: none;
+  }
+`;
