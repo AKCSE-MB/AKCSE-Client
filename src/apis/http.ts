31dcc3943cd5c1edd823384564c259ff80efd019
Jade Lee <jadelee0924@gmail.com>
2025-01-02 21:10:53 -0600

    feat: impl axios interceptors

--- src/apis/http.ts
+++ src/apis/http.ts
@@ -1,10 +1,20 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+import { toast } from 'react-toastify';
 
 class HttpClient {
   public client: AxiosInstance;
+  private requestInterceptorId!: number;
+  private responseInterceptorId!: number;
 
   constructor() {
     this.client = axios.create(this.axiosConfig());
+    this.requestInterceptorId = this.requestInterceptors();
+    this.responseInterceptorId = this.responseInterceptors();
   }
 
   axiosConfig() {
@@ -12,6 +22,49 @@ class HttpClient {
       baseURL: '',
       headers: {},
     };
+  }
+
+  requestInterceptors() {
+    return this.client.interceptors.request.use(
+      (request: InternalAxiosRequestConfig) => {
+        if (typeof window === undefined) return request;
+
+        if (
+          request.headers.Authorization?.toString().split(' ')[1] === 'null'
+        ) {
+          const token = (sessionStorage as Storage).getItem('token');
+          request.headers.Authorization = `Bearer ${token}`;
+          return { ...request };
+        }
+
+        return request;
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (error: any) => {
+        console.log('error', error);
+
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  responseInterceptors() {
+    return this.client.interceptors.response.use(
+      (response: AxiosResponse) => {
+        console.log('axios response!!!!');
+        return response;
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (error: any) => {
+        console.log('error', error.response.data.statusCode);
+
+        if (error.response.data.statusCode >= 400) {
+          toast.error('An error occurred. Please check your request.');
+        }
+
+        return Promise.reject(error);
+      },
+    );
   }
 
   responseBody(response: AxiosResponse) {
@@ -32,6 +85,11 @@ class HttpClient {
 
   async delete<R>(url: string, config?: AxiosRequestConfig) {
     return this.client.delete<R>(url, config).then(this.responseBody);
+  }
+
+  removeInterceptors() {
+    this.client.interceptors.request.eject(this.requestInterceptorId);
+    this.client.interceptors.response.eject(this.responseInterceptorId);
   }
 }
 
