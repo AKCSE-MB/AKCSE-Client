ed94a22792ffbdfc107a957c6ea52fa14a1b8d9e
Jade Lee <jadelee0924@gmail.com>
2025-01-08 14:36:53 -0600

    feat: use util for date and time and update request urls

--- src/apis/events/index.ts
+++ src/apis/events/index.ts
@@ -1,17 +1,16 @@
 import http from '../http';
 import { GetEventsOutput } from '@dev-taeho/akcse_mb/lib/domain/event/dto/event.dto';
 
-export interface EventDisplay extends GetEventsOutput {
-  startDate: string;
-  endDate: string;
-  startTime: string;
-  endTime: string;
-  rsvpDeadlineDate: string;
-  rsvpDeadlineTime: string;
-}
-
 export const getEvents = async () => {
-  const res = await http.get<GetEventsOutput[]>('apis/v1/event');
+  const res = await http.get<GetEventsOutput[]>('/apis/v1/event');
+
+  if (res?.data) {
+    return res?.data;
+  }
+};
+
+export const getEventById = async (id: number) => {
+  const res = await http.get<GetEventsOutput>(`/apis/v1/event/${id}`);
 
   if (res?.data) {
     return res?.data;
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,8 +4,9 @@ import * as S from './page.styled';
 import Header from '@/components/Header/MainHeader';
 import { useEffect, useState } from 'react';
 import { GetEventsOutput } from '@dev-taeho/akcse_mb/lib/domain/event/dto/event.dto';
-import { EventDisplay, getEvents } from '@/apis/events';
+import { getEvents } from '@/apis/events';
 import { useRouter } from 'next/navigation';
+import { getFormattedDate } from '@/utils/formatUtil';
 
 export default function Events() {
   const [events, setEvents] = useState<GetEventsOutput[]>([]);
@@ -22,46 +23,8 @@ export default function Events() {
     fetchEvents();
   }, []);
 
-  const formattedEvents = events.map((event) => ({
-    ...event,
-    startDate: new Date(event.startDateTime).toLocaleDateString('en-US', {
-      month: 'short',
-      day: 'numeric',
-      year: 'numeric',
-    }),
-    endDate: new Date(event.endDateTime).toLocaleDateString('en-US', {
-      month: 'short',
-      day: 'numeric',
-      year: 'numeric',
-    }),
-    startTime: new Date(event.startDateTime).toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-    }),
-    endTime: new Date(event.endDateTime).toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-    }),
-    rsvpDeadlineDate: new Date(event.signUpDeadline).toLocaleDateString(
-      'en-US',
-      {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-      },
-    ),
-    rsvpDeadlineTime: new Date(event.signUpDeadline).toLocaleTimeString(
-      'en-US',
-      {
-        hour: '2-digit',
-        minute: '2-digit',
-      },
-    ),
-  }));
-
-  const handleEventClick = (event: EventDisplay) => {
-    const eventData = encodeURIComponent(JSON.stringify(event));
-    push(`/events/${event.id}?data=${eventData}`);
+  const handleEventClick = (event: GetEventsOutput) => {
+    push(`/events/${event.id}`);
   };
 
   return (
@@ -71,12 +34,12 @@ export default function Events() {
         <S.Title>Upcoming Events</S.Title>
 
         <S.EventList>
-          {formattedEvents.map((event) => (
+          {events.map((event) => (
             <S.EventCard key={event.id} onClick={() => handleEventClick(event)}>
               <S.EventImage src={event.imageUrl} alt={event.title} />
               <S.EventInfo>
                 <S.LocationAndDate>
-                  {event.location} • {event.startDate}
+                  {event.location} • {getFormattedDate(event.startDateTime)}
                 </S.LocationAndDate>
                 <S.EventTitle>{event.title}</S.EventTitle>
               </S.EventInfo>
