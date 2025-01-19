f1dd388072f458c7f28ad98fbba08a6012c7bdcc
Jade Lee <jadelee0924@gmail.com>
2025-01-18 21:26:42 -0600

    feat: only allow login through side navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,10 @@ import { useRouter } from 'next/navigation';
 import * as S from './page.styled';
 import DefaultButton from '@/components/Button/DefaultButton';
 import Header from '@/components/Header/MainHeader';
-import useUser from '@/hooks/useUser';
-import { toast, ToastContainer } from 'react-toastify';
-import { useEffect } from 'react';
-import useKakaoLogin from '@/hooks/useKakaoLogin';
+import { ToastContainer } from 'react-toastify';
 
 export default function Home() {
   const { push } = useRouter();
-  const { isLoggedIn, logout } = useUser();
-  const { loginHandler } = useKakaoLogin();
-
-  useEffect(() => {
-    const successLoginMsg = sessionStorage.getItem('loginMsg');
-    if (successLoginMsg) {
-      toast.success(successLoginMsg);
-      sessionStorage.removeItem('loginMsg');
-    }
-  }, []);
 
   return (
     <S.MainContent>
@@ -33,15 +20,8 @@ export default function Home() {
         <DefaultButton onClick={() => push('/leaderboard')}>
           LEADERBOARD
         </DefaultButton>
-        {isLoggedIn ? (
-          <div>
-            <DefaultButton onClick={logout}>Log Out</DefaultButton>
-            <ToastContainer position="bottom-center" />
-          </div>
-        ) : (
-          <DefaultButton onClick={loginHandler}>LOG IN / ADMIN</DefaultButton>
-        )}
       </S.ButtonContainer>
+      <ToastContainer position="bottom-center" />
     </S.MainContent>
   );
 }
