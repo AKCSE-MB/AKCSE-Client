d2cbd0f4d305c5b824aa5d7c8fa25220f57321de
dev-taeho <cth6809@daum.net>
2025-02-28 22:33:25 -0600

    feat: impl sidebar component

---
+++ src/components/c-side-menu/page.styled.ts
@@ -0,0 +1,63 @@
+import styled, { css, keyframes } from 'styled-components';
+
+export const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+export const Container = styled.div<{ open: boolean }>`
+  width: 100%;
+  max-width: 390px;
+  height: 100%;
+  background-color: #ffff;
+  top: 0;
+  left: 0;
+  position: fixed;
+  overflow: hidden;
+  ${({ open }) => sideBarSettings(open)}
+
+  @media screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;
+
+const sideBarSettings = (visible: boolean) => css`
+  visibility: ${visible ? 'visible' : 'hidden'};
+  z-index: 15;
+  animation: ${visible ? fadeIn : fadeOut} 0.4s ease-in-out;
+  transition: visibility 0.4s ease-in-out;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+  margin-top: 122px;
+  overflow: hidden;
+`;
+
+export const MenuItem = styled.div`
+  height: 29px;
+  padding: 0 48px;
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 44px;
+  color: #39170e;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+`;
--- src/components/layout/mobile-layout/page.styled.ts
+++ src/components/layout/mobile-layout/page.styled.ts
@@ -12,6 +12,7 @@ export const Layout = styled.div`
   margin: 0 auto;
   width: 100%;
   background-color: rgb(255, 251, 246);
+  overflow: hidden;
 
   @media screen and (max-width: 768px) {
     max-width: 100%;
