8dcf04b50db9af0a4f67dbaf0e9d39ab4333743d
Jade Lee <jadelee0924@gmail.com>
2025-03-07 15:42:09 -0600

    feat: update to use a single API call to get all events, then handle them appropriately

--- src/apis/events/index.ts
+++ src/apis/events/index.ts
@@ -1,8 +1,8 @@
 import http from '../http';
 import { GetEventsOutput } from '@dev-taeho/akcse_mb/lib/domain/event/dto/event.dto';
 
-export const getUpcomingEvents = async () => {
-  const res = await http.get<GetEventsOutput[]>('/apis/v1/event/upcoming');
+export const getEvents = async () => {
+  const res = await http.get<GetEventsOutput[]>('/apis/v1/event');
 
   if (res?.data) {
     return res?.data;
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,43 +4,30 @@ import * as S from './page.styled';
 import CHeader from '@/components/c-header';
 import { useEffect, useState } from 'react';
 import { GetEventsOutput } from '@dev-taeho/akcse_mb/lib/domain/event/dto/event.dto';
-import { getUpcomingEvents, getPastEvents } from '@/apis/events';
+import { getEvents } from '@/apis/events';
 import { getFormattedDate } from '@/utils/formatUtil';
 import CEventItem from '@/components/c-event-item';
 import DefaultButton from '@/components/Button/DefaultButton';
 import CFooter from '@/components/c-footer';
+import { useRouter } from 'next/navigation';
 
 export default function Events() {
-  const [upcomingEvents, setUpcomingEvents] = useState<GetEventsOutput[]>([]);
-  const [pastEvents, setPastEvents] = useState<GetEventsOutput[]>([]);
-  const [showMore, setShowMore] = useState<boolean>(false);
+  const [upcoming, setUpcoming] = useState<GetEventsOutput[]>([]);
+  const [past, setPast] = useState<GetEventsOutput[]>([]);
+
+  const { push } = useRouter();
 
   useEffect(() => {
-    const fetchUpcomingEvents = async () => {
-      const events = await getUpcomingEvents();
+    const fetchEvents = async () => {
+      const events = await getEvents();
       if (events) {
-        setUpcomingEvents(events);
+        setUpcoming(events.upcoming);
+        setPast(events.past);
       }
     };
 
-    const fetchPastEvents = async () => {
-      const events = await getPastEvents();
-      if (events) {
-        setPastEvents(events);
-      }
-    };
-
-    fetchUpcomingEvents();
-    fetchPastEvents();
+    fetchEvents();
   }, []);
-
-  const handleMoreButton = () => {
-    setShowMore(true);
-  };
-
-  const handleLessButton = () => {
-    setShowMore(false);
-  };
 
   return (
     <S.MainContainer>
@@ -49,7 +36,7 @@ export default function Events() {
         <S.Title>Upcoming Events</S.Title>
 
         <S.EventList>
-          {upcomingEvents.map((event) => (
+          {upcoming.map((event) => (
             <CEventItem
               title={event.title}
               date={getFormattedDate(event.startDateTime)}
@@ -59,6 +46,7 @@ export default function Events() {
                 text: 'Learn More',
                 route: `/events/${event.id}`,
               }}
+              key={event.id}
             ></CEventItem>
           ))}
         </S.EventList>
@@ -67,58 +55,25 @@ export default function Events() {
       <S.PastEvents>
         <S.Title>Past Events</S.Title>
 
-        {pastEvents.length > 0 && (
-          <S.EventList>
+        <S.EventList>
+          {past.slice(0, 3).map((event) => (
             <CEventItem
-              title={pastEvents[0]?.title}
-              date={getFormattedDate(pastEvents[0]?.startDateTime)}
-              description={pastEvents[0]?.description}
-              bgUrl={pastEvents[0]?.imageUrl}
+              title={event?.title}
+              date={getFormattedDate(event?.startDateTime)}
+              description={event?.description}
+              bgUrl={event?.imageUrl}
               link={{
                 text: 'Learn More',
-                route: `/events/${pastEvents[0]?.id}`,
+                route: `/events/${event?.id}`,
               }}
+              key={event.id}
             />
+          ))}
 
-            <CEventItem
-              title={pastEvents[1]?.title}
-              date={getFormattedDate(pastEvents[1]?.startDateTime)}
-              description={pastEvents[1]?.description}
-              bgUrl={pastEvents[1]?.imageUrl}
-              link={{
-                text: 'Learn More',
-                route: `/events/${pastEvents[1]?.id}`,
-              }}
-            ></CEventItem>
-
-            {!showMore && (
-              <DefaultButton onClick={handleMoreButton}>
-                View More
-              </DefaultButton>
-            )}
-
-            {showMore && (
-              <S.morePastEvents>
-                {pastEvents.slice(2).map((event) => (
-                  <CEventItem
-                    title={event.title}
-                    date={getFormattedDate(event.startDateTime)}
-                    description={event.description}
-                    bgUrl={event.imageUrl}
-                    link={{
-                      text: 'Learn More',
-                      route: `/events/${event.id}`,
-                    }}
-                  />
-                ))}
-
-                <DefaultButton onClick={handleLessButton}>
-                  View Less
-                </DefaultButton>
-              </S.morePastEvents>
-            )}
-          </S.EventList>
-        )}
+          <DefaultButton onClick={() => push('/events/past')}>
+            View More
+          </DefaultButton>
+        </S.EventList>
       </S.PastEvents>
 
       <CFooter />
