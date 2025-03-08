959a7f7711e46997844680a88b12189d2fa95ed0
dev-taeho <cth6809@daum.net>
2025-03-08 09:44:51 -0600

    faet: add modal in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,46 @@ import DefaultButton from '@/components/Button/DefaultButton';
 import CHeader from '@/components/c-header';
 import { ToastContainer } from 'react-toastify';
 import CFooter from '@/components/c-footer';
+import { useModalStore } from '@/store/useModalStore';
+import { MODAL_TYPES } from '@/components/Modal/GlobalModal';
+import useUser from '@/hooks/useUser';
+import useKakaoLogin from '@/hooks/useKakaoLogin';
+import CAUTION from '@/assets/common/logo/caution.svg';
 
 export default function Home() {
-  const { push } = useRouter();
+  const router = useRouter();
+  const { openModal, closeModal } = useModalStore();
+  const { isLoggedIn } = useUser();
+  const { loginHandler } = useKakaoLogin();
+
+  const loginInfoModal = () => {
+    openModal(MODAL_TYPES.dialog, {
+      message: 'You must be Akcse memeber to access this page',
+      logo: <CAUTION />,
+      handleConfirm: () => loginHandler(),
+      handleClose: () => closeModal(MODAL_TYPES.dialog),
+    });
+  };
+
+  const onLeaderBoardClick = () => {
+    if (!isLoggedIn) return loginInfoModal();
+
+    router.push('/leaderboard');
+  };
 
   return (
     <S.PageWrapper>
       <S.MainContent>
         <CHeader />
         <S.ButtonContainer>
-          <DefaultButton onClick={() => push('/')}>HOME</DefaultButton>
-          <DefaultButton onClick={() => push('/events')}>EVENTS</DefaultButton>
-          <DefaultButton onClick={() => push('/')}>RESOURCES</DefaultButton>
-          <DefaultButton onClick={() => push('/leaderboard')}>
+          <DefaultButton onClick={() => router.push('/')}>HOME</DefaultButton>
+          <DefaultButton onClick={() => router.push('/events')}>
+            EVENTS
+          </DefaultButton>
+          <DefaultButton onClick={() => router.push('/')}>
+            RESOURCES
+          </DefaultButton>
+          <DefaultButton onClick={onLeaderBoardClick}>
             LEADERBOARD
           </DefaultButton>
         </S.ButtonContainer>
