40fac4c5a0b8ac2a8fd528b451f0b67c9b9ee748
Jade Lee <jadelee0924@gmail.com>
2025-03-08 15:51:10 -0600

    feat: impl past events page

---
+++ src/app/events/past/page.styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding-top: 100px;
+`;
+
+export const Title = styled.h1`
+  font-weight: 600;
+  text-align: left;
+  font-size: 36px;
+  padding: 15px;
+  margin-bottom: 33px;
+`;
+
+export const EventList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  gap: 24px;
+`;
+
+export const PastEvents = styled.div`
+  margin-top: 80px;
+  margin-bottom: 80px;
+`;
