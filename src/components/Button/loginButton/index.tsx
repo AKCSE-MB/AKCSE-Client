d473312813dea05291b8dfa43a59712363e7014a
dev-taeho <cth6809@daum.net>
2025-03-20 19:07:04 -0500

    fix: correct buttonAttribute props

--- src/components/Button/loginButton/index.tsx
+++ src/components/Button/loginButton/index.tsx
@@ -1,9 +1,12 @@
+import { ButtonHTMLAttributes } from 'react';
 import * as S from './page.styled';
 import KAKAO from '@/assets/common/logo/kakao.svg';
 
-export default function LoginButton() {
+export default function LoginButton(
+  props: ButtonHTMLAttributes<HTMLButtonElement>,
+) {
   return (
-    <S.Button>
+    <S.Button {...props}>
       <KAKAO width={20} height={20} />
       Login with Kakao
     </S.Button>
