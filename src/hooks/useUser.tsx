3efb7bc79d0f82c6494ecaf74b3f2d7ee88cd607
Jade Lee <jadelee0924@gmail.com>
2025-03-24 16:18:09 -0500

    feat: display modal on user's first login attempt

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -3,6 +3,10 @@ import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { AxiosError } from 'axios';
+import { useModalStore } from '@/store/useModalStore';
+import { MODAL_TYPES } from '@/components/Modal/GlobalModal';
+import CAUTION from '@/assets/common/logo/caution.svg';
 
 interface UseUserReturn {
   isLoggedIn: boolean;
@@ -14,6 +18,16 @@ export default function useUser(): UseUserReturn {
   const { push } = useRouter();
   const tokenName = 'token';
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const { openModal, closeModal } = useModalStore();
+  const loginInfoModal = (message: string) => {
+    openModal(MODAL_TYPES.dialog, {
+      message: message,
+      logo: <CAUTION />,
+      handleConfirm: () => closeModal(MODAL_TYPES.dialog),
+      handleClose: () => closeModal(MODAL_TYPES.dialog),
+      isLogin: false,
+    });
+  };
 
   useEffect(() => {
     const token = sessionStorage.getItem(tokenName);
@@ -28,9 +42,18 @@ export default function useUser(): UseUserReturn {
       push('/');
     },
     onError: (error) => {
-      console.error('Login failed:', error);
-      setIsLoggedIn(false);
-      toast.error('Log In Failed, Please Try Again!');
+      const axiosError = error as AxiosError;
+
+      if (axiosError.response?.status === 401) {
+        const message =
+          'Account successfully registered. Please wait for access approval.';
+        loginInfoModal(message);
+        push('/');
+      } else {
+        console.error('Login failed:', error);
+        setIsLoggedIn(false);
+        toast.error('Log In Failed, Please Try Again!');
+      }
     },
   });
 
