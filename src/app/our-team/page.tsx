fd8dbe43fe566458dd887203728c2921a682a6b5
dev-taeho <cth6809@daum.net>
2025-10-22 20:59:59 -0500

    chore: update our team page styling - move max-width to wrapper from inner container
    <description>

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -3,12 +3,12 @@ import ExecutiveCard from '@/components/c-executive-card';
 
 export default function ExecutiveList() {
   return (
-    <div className="w-full px-4 py-10">
+    <div className="mx-auto max-w-[1050px] px-4 py-10">
       <h1 className="mb-[33px] text-left text-[36px] font-semibold text-[#39170e]">
         Our Team
       </h1>
 
-      <div className="mx-auto grid max-w-[1050px] grid-cols-1 gap-6 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
         {executives.map((executive) => (
           <ExecutiveCard key={executive.id} {...executive} />
         ))}
