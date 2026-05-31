604eda65df506c10b27e939a986a50f839cf98de
dev-taeho <cth6809@daum.net>
2026-05-31 11:39:47 -0500

    chore: remove deprectaed field in next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   compiler: {
     styledComponents: true,
   },
