b20f42405cd60e9fc0dfb66afbe3eb91d708cbd1
dev-taeho <cth6809@daum.net>
2026-06-01 09:47:14 -0500

    chore: remove unused dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,37 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginImport from "eslint-plugin-import";
+import prettier from "eslint-config-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  {settings: {
-    react: {
-      version: "detect",
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    settings: {
+      react: { version: "detect" },
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
     },
-  }},
-  {rules: {
-    "react/react-in-jsx-scope": "off",
-  }},
+  },
   ...tseslint.configs.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    plugins: { "react-hooks": pluginReactHooks },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    plugins: { import: pluginImport },
+    rules: {
+      "import/no-duplicates": "error",
+      "import/no-unused-modules": "warn",
+    },
+  },
+  prettier,
 ];
