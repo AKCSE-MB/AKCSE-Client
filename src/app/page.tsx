067cd03d91e2940f9e59fbdc793bcfba85e2e0c8
miniminikim <nanaer94@gmail.com>
2026-08-21 11:17:28 -0700

    remove research section from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import EventsSection from '@/components/home/EventsSection';
 import HeroSection from '@/components/home/HeroSection';
 import InfoSection from '@/components/home/InfoSection';
 import NewsSection from '@/components/home/NewsSection';
-import ResearchSection from '@/components/home/ResearchSection';
 import TeamSection from '@/components/home/TeamSection';
 import * as S from './page.styled';
 
@@ -16,7 +15,6 @@ export default function Home() {
       <EventsSection />
       <TeamSection />
       <NewsSection />
-      <ResearchSection />
     </S.PageWrapper>
   );
 }
