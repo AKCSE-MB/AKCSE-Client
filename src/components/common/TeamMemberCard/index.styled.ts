df732f90fb027c780c41ce0e65bbff76b12f2f4c
miniminikim <nanaer94@gmail.com>
2026-08-21 12:08:10 -0700

    remove gray scale from team member photo card

--- src/components/common/TeamMemberCard/index.styled.ts
+++ src/components/common/TeamMemberCard/index.styled.ts
@@ -28,7 +28,6 @@ export const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  filter: grayscale(100%);
   transition:
     filter 1000ms ease-out,
     transform 1000ms ease-out;
