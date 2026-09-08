5d95f399d415e30ae1bbfa8be22de645d8e0ccda
dev-taeho <cth6809@daum.net>
2026-09-07 20:25:01 -0500

    chore: correct join link

--- src/components/layout/MainLayout/index.tsx
+++ src/components/layout/MainLayout/index.tsx
@@ -45,7 +45,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
     name: 'Join Us',
     clickEvent: () =>
       window.open(
-        'https://docs.google.com/forms/d/e/1FAIpQLSd2pIattHv1NxclVVHLMVRQD5TiqvuGJp1nAY_K5YCrBpLcvQ/viewform',
+        'https://docs.google.com/forms/d/e/1FAIpQLSdDb6vLL-WMoAltOlxGfXJufiis3M5tA0bl4tKOdbm4DrjMgA/viewform?usp=dialog',
       ),
   };
 
